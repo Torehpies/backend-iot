927fc5b183665f9a31bb3e94bfe6cc5ace7f1512
Torehpies <salupadoseiffer@gmail.com>
2024-12-18 10:22:39 +0800

    Added cors and helmet

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors'; // Import cors
 import dataRoutes from './routes/data.routes';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 import authRoutes from './routes/auth.routes';
 
 dotenv.config(); // Ensure dotenv is configured at the top
@@ -11,7 +12,20 @@ dotenv.config(); // Ensure dotenv is configured at the top
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); // Enable CORS
+app.use(helmet())
+
+app.use(cors({
+	origin: 'https://iotwebsys.shop',
+	methods: 'GET,POST,PUT,DELETE',
+	allowedHeaders: 'Content-Type,Authorization'
+})); // Enable CORS
+
+app.use((req, res, next) => {
+	res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+	res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+	next();
+  });
+
 app.use(bodyParser.json());
 app.use('/api/data', dataRoutes);
 app.use('/api/auth', authRoutes);
